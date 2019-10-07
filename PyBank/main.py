3a8d7b7c59e3a171215a40aed36aadadcffc551d
chuhaovince <53956182+chuhaovince@users.noreply.github.com>
2019-10-06 22:45:52 -0400

    PyBank average changes not finished, PyPoll finished
    Added comments to the code, change output text file name for PyBank analysis, deleted some redundant codes for PyBank
    
    Still working on PyBank: average changes

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,6 +1,7 @@
 # import modules in python
 import pandas as pd
 import os
+
 # Open pybank csv file with pandas dataframe
 filepath = os.path.join("PyBank","budget_data.csv")
 budget_data = pd.read_csv(filepath)
@@ -10,13 +11,14 @@ total_mon = len(budget_data)
 
 # the net total amount over the entire period
 net_profit = sum(budget_data["Profit/Losses"])
-print(net_profit)
+
 
 # the average of the changes over the entire period
 avg_changes = net_profit/total_mon
 
 # initiate a list for step increases
 inc = []
+
 # the greatest increase in profits (date and amount) over the entire period
 for i in range(total_mon-1):
     difference = budget_data["Profit/Losses"][i + 1] - budget_data["Profit/Losses"][i]
@@ -26,15 +28,17 @@ for i in range(total_mon-1):
 maximun_increase = max(inc)
 maximun_decrease = min(inc)
 # find corresponding month for maximun increase and decrease in profits
-print(inc.index(maximun_increase))
 max_month = budget_data["Date"][inc.index(maximun_increase)+1]
 min_month = budget_data["Date"][inc.index(maximun_decrease)+1]
 
+# variable assignment (results with strings for print-out)
 fncl_anls = f'Financial Analysis\n----------------------'
 tol_mon = f'Total Months: {total_mon}'
 total = f'Total: ${net_profit}'
 avg_chan = f'Average Changes: $'
 profits_in_de = f'Greatest Increase in Porfits: {max_month} (${maximun_increase})\nGreatest Decrease in Profits: {min_month} (${maximun_decrease})'
+
+#print results as described
 print(fncl_anls)
 print(tol_mon)
 print(total)
@@ -45,9 +49,11 @@ print(profits_in_de)
 results_pybank = [fncl_anls,tol_mon,total,avg_chan,profits_in_de]
 
 # write financial analysis results into a new text file
-with open("PyBank\PyBank_Financial_Analysis_Results.txt", "w", newline = "") as results:
+with open("PyBank\\PyBank_Results.txt", "w", newline = "") as results:
     for i in results_pybank:
         results.write(i)
         results.write('\n')
 # or use results.writelines to write the list results_pybank
+
+#close text file
 results.close()
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -3,38 +3,41 @@ import os
 
 # get the data and transfer it to a pandas dataframe
 filepath = os.path.join("PyPoll","election_data.csv")
-
 election_data = pd.read_csv(filepath)
 
-# total votes
+# total votes of all the candidates
 total_votes = len(election_data)
 
-#convert the candidate list into a set so i can know the candidates' names for following analysis
+#convert the candidate list into a set to filter the unique candidates' names for following analysis
 candidate_set = set(election_data["Candidate"])
 
-# convert the set to a list again so i have a list of all unique names
+# convert the set to a list again for further analysis
 candidate_list = list(candidate_set)
 
-print(f'Election Results\n-----------------------')
+print(f'Election Results\n----------------------------')
 
 # write results in file, create a new file in the same directory
-results = open("PyPoll\Election_Results.txt","w")
-results.write(f'Election Results\n-----------------------\n')
+results = open("PyPoll\\Election_Results.txt","w")
+results.write(f'Election Results\n----------------------------\n')
 
-#variable to compare percentage amoung candidates
+# variable to compare percentage amoung candidates
 p = 0
+
+# for loop to analyze each candidate in the list, and print info for each candidate 
+# as well as to write the results into the text file
 for i in range(len(candidate_list)):
-    candidate = election_data["Candidate"][election_data["Candidate"]==candidate_list[i]]
-    total_candidate_votes = len(candidate)
-    percent = total_candidate_votes/total_votes*100
-    if percent > p:
+    candidate = election_data["Candidate"][election_data["Candidate"]==candidate_list[i]] # filter out the votes of unique candidate and store it as a list
+    total_candidate_votes = len(candidate) # get the total votes of this candidate
+    percent = total_candidate_votes/total_votes*100 # calculate the percentage of the vote this candidate got
+    if percent > p:   # if statement to find the higher voted candidate and his/her name
         p = percent
         candidate_name = candidate_list[i]
     print(f'{candidate_list[i]}: {percent:.3f}% ({total_candidate_votes})')
     results.write(f'{candidate_list[i]}: {percent:.3f}% ({total_candidate_votes})\n')
-print(f'-----------------------')
+print(f'----------------------------')
 print(f'Winner: {candidate_name}')
-results.write(f'-----------------------\n')
+results.write(f'----------------------------\n')
 results.write(f'Winner: {candidate_name}\n')
 
+# close file
 results.close()
